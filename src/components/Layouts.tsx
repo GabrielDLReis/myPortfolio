a4a21123fb1f82249311c7e832218c946bde5613
GabrielDLR <gabrieldilucciareis@hotmail.com>
2022-08-14 01:15:18 -0300

    All components are imported here to have a clean code. After this, we export this component to App.tsx

---
+++ src/components/Layouts.tsx
@@ -0,0 +1,32 @@
+import "../styles/footer.css"
+import '../styles/background.css'
+import '../styles/topcontent.css'
+import '../styles/navbar.css'
+import '../styles/myprojects.css'
+
+import NavBar from './NavBar'
+import TopContent from './TopContent'
+import BackgroundImage from './BackgroundImage'
+import MyProjects from './MyProjects'
+import HardSkills from "./HardSkills"
+import Footer from "./Footer"
+
+
+interface Props {
+  themeChanger : boolean
+}
+
+function Layouts({themeChanger} : Props) {
+  return <div>
+    <NavBar aButton={themeChanger}/>
+    <TopContent themeChanger={themeChanger}/>
+    <BackgroundImage />
+    <MyProjects themeChanger={themeChanger}/>
+    <HardSkills />
+    <Footer themeChanger={themeChanger}/>
+
+
+  </div>
+}
+
+export default Layouts
