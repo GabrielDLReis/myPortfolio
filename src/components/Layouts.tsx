2b58e12509eff4247d43e4eaf7ecf338e6ffcbeb
GabrielDLR <gabrieldilucciareis@hotmail.com>
2022-08-16 23:13:16 -0300

    Added grid template to projects and, my projects title has been removed to suffer edits

--- src/components/Layouts.tsx
+++ src/components/Layouts.tsx
@@ -2,15 +2,17 @@ import "../styles/footer.css"
 import '../styles/background.css'
 import '../styles/topcontent.css'
 import '../styles/navbar.css'
-import '../styles/myprojects.css'
+
 import "../styles/hardskills.css"
+import backgroundLow from "../assets/img/lowResolution.jpg"
 
 import NavBar from './NavBar'
 import TopContent from './TopContent'
 import BackgroundImage from './BackgroundImage'
-import MyProjects from './MyProjects'
+
 import HardSkills from "./HardSkills"
 import Footer from "./Footer"
+import MyTestProjects from "./myTestProjects"
 
 
 interface Props {
@@ -22,7 +24,7 @@ function Layouts({themeChanger} : Props) {
     <NavBar aButton={themeChanger}/>
     <TopContent themeChanger={themeChanger}/>
     <BackgroundImage />
-    <MyProjects themeChanger={themeChanger}/>
+    <MyTestProjects themeChanger={themeChanger} backgroundLow={backgroundLow}/>
     <HardSkills themeChanger={themeChanger}/>
     <Footer themeChanger={themeChanger}/>
     {/* < themeChanger={themeChanger}/> */}
--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -1,5 +1,6 @@
 import  BackgroundLowRes  from "../assets/img/lowResolution.jpg"
 import BackgroundImage from "./BackgroundImage"
+import "../styles/myprojects.css"
 
 interface Props {
   themeChanger : boolean
@@ -9,41 +10,15 @@ interface Props {
 }
 
 function MyProjects ({imageProject, text, title, themeChanger} : Props) {
-  return <div >
-    <div className="projectsTitle">
-      <h2>My projects</h2>
-      <p> All these projects are my own</p>
-    </div>
+  return <div className="gridTest">
+    
     
     <div className="containerImages">
       <div className="grid">
         <div className={themeChanger ? "black" : "white"}>
-          <img src={imageProject} alt="Meu projeto 1" />
-          
-          <h4>{title}</h4>
-          <p>{text}</p>
-        </div>
-        <div className={themeChanger ? "black" : "white"}>
-          <img className="imageProjects" src={BackgroundLowRes} alt="Meu projeto 3" />
-          <h4>Projeto 1</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consequatur error molestias consectetur quam est beatae.</p>
-        </div>
-        <div className={themeChanger ? "black" : "white"}>
-          <img className="imageProjects" src={BackgroundLowRes} alt="Meu projeto 4" />
-          <h4>Projeto 1</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consequatur error molestias consectetur quam est beatae.</p>
-        </div>
-        <div className={themeChanger ? "black" : "white"}>
-          <img className="imageProjects" src={BackgroundLowRes} alt="Meu projeto 5" />
-          <h4>Projeto 1</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consequatur error molestias consectetur quam est beatae.</p>
-        </div>
-        <div className={themeChanger ? "black" : "white"}>
-          <img className="imageProjects" src={BackgroundLowRes} alt="Meu projeto 6" />
-          <h4>Projeto 1</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae consequatur error molestias consectetur quam est beatae.</p>
-
-          
+          <img src={imageProject} alt="Meu projeto 1" className="imageProject" />
+          <h4 className="titleOfProjects">{title}</h4>
+          <p className="textOfProject">{text}</p>
         </div>
       </div>
     </div>
